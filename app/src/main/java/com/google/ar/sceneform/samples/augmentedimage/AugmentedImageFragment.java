8c48d7c6b6f79a4430708a17494f1603d38ffded
kyczawon <lkn16@ic.ac.uk>
2019-01-26 15:14:55 +0000

    add new menu

--- app/src/main/java/com/google/ar/sceneform/samples/augmentedimage/AugmentedImageFragment.java
+++ app/src/main/java/com/google/ar/sceneform/samples/augmentedimage/AugmentedImageFragment.java
@@ -45,10 +45,10 @@ public class AugmentedImageFragment extends ArFragment {
   // This is the name of the image in the sample database.  A copy of the image is in the assets
   // directory.  Opening this image on your computer is a good quick way to test the augmented image
   // matching.
-  private static final String DEFAULT_IMAGE_NAME = "default.jpg";
+  private static final String DEFAULT_IMAGE_NAME = "square.jpg";
 
   // This is a pre-created database containing the sample image.
-  private static final String SAMPLE_IMAGE_DATABASE = "sample_database.imgdb";
+  private static final String SAMPLE_IMAGE_DATABASE = "db.imgdb";
 
   // Augmented image configuration and rendering.
   // Load a single image (true) or a pre-generated image database (false).
